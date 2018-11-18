ad2290a6a83940f7689625e76f3830929d6f989d
Ellirid-PrismAI <dmitriy.kupriienko@prism.com>
2018-11-18 21:50:50 +0200

    Add permissions to statistics view

--- cvat/apps/stats/views.py
+++ cvat/apps/stats/views.py
@@ -6,11 +6,13 @@ from cvat.apps.authentication.decorators import login_required
 from cvat.apps.stats.services import collect_annotators_stats
 
 
+@user_passes_test(lambda u: u.is_staff, '/admin')
+@login_required
 def stats_index_view(request):
     return render(request, 'stats/index.html')
 
 
-@user_passes_test(lambda u: u.is_staff)
+@user_passes_test(lambda u: u.is_staff, '/admin')
 @login_required
 def get_operators_stats_view(request):
     stats = collect_annotators_stats()
