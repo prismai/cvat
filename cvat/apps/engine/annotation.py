764ae0dfce9d35f8ed5862d8de9a1d21b4fdec73
Alexei Andrushievich <oleksii.andrushievich@prism.com>
2019-06-25 13:39:57 +0300

    Revert annotations track manager dumper assert for frames order

--- cvat/apps/engine/annotation.py
+++ cvat/apps/engine/annotation.py
@@ -1128,8 +1128,7 @@ class TrackManager(ObjectManager):
         prev_shape = {}
         for shape in track["shapes"]:
             if prev_shape:
-                # TODO: check WTF!!1
-                # assert shape["frame"] > curr_frame
+                assert shape["frame"] > curr_frame
                 for attr in prev_shape["attributes"]:
                     if attr["spec_id"] not in map(lambda el: el["spec_id"], shape["attributes"]):
                         shape["attributes"].append(copy.deepcopy(attr))
